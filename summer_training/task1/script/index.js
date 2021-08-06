4912f1921335c176eae0a0938fd1376b5c0a5f33
anh071299 <74811191+anh071299@users.noreply.github.com>
2021-08-06 22:00:17 +0700

    'ok'

--- summer_training/task1/script/index.js
+++ summer_training/task1/script/index.js
@@ -1,3 +1,4 @@
+// menu expands
 const menuToggle = $('.header__navbar-toggle');
 const toggle = $('.header__icon-list');
 $(window).mouseup(function (e) {
@@ -14,7 +15,7 @@ toggle.on('click', () => {
 $('.header__icon-close').on('click', () => {
     menuToggle.toggleClass('is-active');
 })
-
+// econonic-tabs
 $('ul.main__economic-tabs li a').click(function(){
     $this = $(this);
     $('.main__economic-tab-content').hide();
@@ -23,5 +24,8 @@ $('ul.main__economic-tabs li a').click(function(){
     var panel = $this.attr('href');
     $(panel).show();
     return false;
-  });//end click
+  });
   $('ul.main__economic-tabs li:first a').click();
+// header
+let linkIndex = location.pathname.split("/").length;
+  $('.main__navbar-menu a[href^="' + location.pathname.split("/")[linkIndex - 1] + '"]').addClass('active');
